ad1f412df19ff9a790cf48f727bc7082034076d8
sheermoon <sheermood@gmail.com>
2021-06-20 11:25:28 +0300

    Initial commit

---
+++ server_cloud/src/test/java/com/example/server_cloud/ServerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
